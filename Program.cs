0f5df1884ead12ab7c73b1670e26429d85c8194e
Mikkydice <Oibohdestiny4@gmail.com>
2022-04-14 12:53:35 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poem_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string FirstColour, SecondColour, Crush ; 
+
+            Console.Write ( " Enter a colour: " );
+            FirstColour = (Console.ReadLine());
+
+            Console.Write ( " Enter another colour: " );
+            SecondColour = (Console.ReadLine());
+
+            Console.Write ( " Enter name of your crush: " );
+            Crush = (Console.ReadLine());
+
+
+            Console.WriteLine(" Roses are " + FirstColour );
+            Console.WriteLine( " Violet are " + SecondColour );
+            Console.WriteLine(" I love " + Crush );
+
+            Console.ReadLine();
+        }
+    }
+}
